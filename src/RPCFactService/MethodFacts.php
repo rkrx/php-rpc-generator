d43f64e5a661616f60f90ce429ecf925070847a5
rkr <ron.kirschler@gmail.com>
2024-03-23 16:16:25 +0100

    Enhanced RPCClassMethodFactProvider

--- src/RPCFactService/MethodFacts.php
+++ src/RPCFactService/MethodFacts.php
@@ -3,6 +3,15 @@
 namespace RpcGenerator\RPCFactService;
 
 class MethodFacts {
+	/**
+	 * @param string $module
+	 * @param string $name
+	 * @param string $function
+	 * @param class-string $className
+	 * @param string $classMethodName
+	 * @param string|null $requiredClassRight
+	 * @param string|null $requiredMethodRight
+	 */
 	public function __construct(
 		public string $module,
 		public string $name,
